decdd39533673b5d4eeda03025b137a0b9f58d32
Evadne Wu <ev@monoceroi.com>
2012-07-11 18:50:54 +0800

    initial scaffold

---
+++ IRAQPhotoPickerController-Sample/IRAQPPCSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IRAQPPCSAppDelegate.h
+//  IRAQPhotoPickerController-Sample
+//
+//  Created by Evadne Wu on 7/11/12.
+//  Copyright (c) 2012 Iridia Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IRAQPPCSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IRAQPhotoPickerController-Sample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IRAQPhotoPickerController-Sample
+//
+//  Created by Evadne Wu on 7/11/12.
+//  Copyright (c) 2012 Iridia Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IRAQPPCSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([IRAQPPCSAppDelegate class]));
+	}
+}
